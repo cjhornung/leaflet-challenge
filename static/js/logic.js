4cff50a42cd9a12194dfed1fec3db87a1f48cc6d
cjhornung <cjhornung@gmail.com>
2023-09-24 19:11:02 -0500

    Added Legend to Map

--- static/js/logic.js
+++ static/js/logic.js
@@ -13,10 +13,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //Use the D3 library to read in samples.json from the URL:
 const geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson";
 
-// Promise Pending
-const dataPromise = d3.json(geoData);
-console.log("Data Promise: ", dataPromise);
-
 
 // Fetch the JSON data and console log it
 let metaData;
@@ -29,8 +25,8 @@ d3.json(geoData).then(function(data) {
 
     // Loop through the features array, and create one marker for each features object.
     for (let i = 0; i < features.length; i++) {
-        console.log(features)
         let coordinates= [features[i].geometry.coordinates[1],features[i].geometry.coordinates[0]]
+        // Set marker color based off depth (km)
         if (features[i].geometry.coordinates[2]<10)
         {
             fillColorVar="#A3F600";
@@ -54,14 +50,41 @@ d3.json(geoData).then(function(data) {
         fillOpacity: 0.75,
         color:"white",
         fillColor: fillColorVar,
-        // Setting our circle's radius to equal the output of our markerSize() function:
-        // This will make our marker's size proportionate to its population.
+        // This will make our marker's size proportionate to its magnitude.
         radius: features[i].properties.mag*10000
         }).bindPopup(`<h3>Magnitude: ${features[i].properties.mag}</h3> <h3>Location: ${features[i].properties.place}</h3> <h3>Depth: ${features[i].geometry.coordinates[2]} km</h3>`).addTo(myMap);
      }
+    // Define an array with legend colors and labels
+    var legendData = [
+        { color: "#A3F600", label: "0-10 km" },
+        { color: "#DCF400", label: "10-30 km" },
+        { color: "#F7DB11", label: "30-50 km" },
+        { color: "#FDB72A", label: "50-70 km" },
+        { color: "#FCA35D", label: "70-90 km" },
+        { color: "#FF5F65", label: "90+ km" }
+    ]; 
+     // Define a legend control
+    var legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend');
+        var labels = [];
+        for (var i = 0; i < legendData.length; i++) {
+            labels.push(
+                '<i style="background-color:' + legendData[i].color + '">' + legendData[i].label+'</i> '
+            );
+        }
+        console.log(labels)
+        div.innerHTML = labels.join('<br>');
+        return div;
+    };
+
+    // Add the legend to the map
+    legend.addTo(myMap);
 
 
+
+     
 
 
 });
-
